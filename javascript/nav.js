c0477523f38d947a556d2062639c11df7f131dee
gandh99 <gandh99@hotmail.com>
2020-01-05 21:28:07 +0800

    Styled the hamburger icon for opening and closing. Implemented the open and close function

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,3 +1,16 @@
+var navIsOpen = false;
+
+function openCloseNav(x) {
+    transformNavIcon(x);
+    if (navIsOpen) {
+        closeNav();
+        navIsOpen = false;
+    } else {
+        openNav();
+        navIsOpen = true;
+    }
+}
+
 function transformNavIcon(x) {
     x.classList.toggle("change");
 }
@@ -8,10 +21,22 @@ function openNav() {
     document.getElementById("nav-menu-container").style.backgroundColor = "transparent";
 
     // Style the hamburger icon
+   hamburgerClose();
+}
+
+function closeNav() {
+    document.getElementById("nav-drawer-container").style.height = "0";
+    hamburgerOpen();
+}
+
+function hamburgerClose() {
     document.getElementById("nav-bar1").style.backgroundColor = "black";
     document.getElementById("nav-bar3").style.backgroundColor = "black";
 }
 
-function closeNav() {
-    document.getElementById("nav-drawer-container").style.width = "0";
+function hamburgerOpen() {
+    document.getElementById("nav-menu-container").style.backgroundColor = "#434242";
+    document.getElementById("nav-bar1").style.backgroundColor = "white";
+    document.getElementById("nav-bar2").style.backgroundColor = "white";
+    document.getElementById("nav-bar3").style.backgroundColor = "white";
 }
