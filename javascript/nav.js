98f37e5487ee888cd3bb269177924b405ae36af0
gandh99 <gandh99@hotmail.com>
2020-01-05 21:09:46 +0800

    Added content to the nav drawer

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,3 +1,17 @@
-function transformNav(x) {
+function transformNavIcon(x) {
     x.classList.toggle("change");
+}
+
+function openNav() {
+    // Open the nav drawer
+    document.getElementById("nav-drawer-container").style.height = "auto";
+    document.getElementById("nav-menu-container").style.backgroundColor = "transparent";
+
+    // Style the hamburger icon
+    document.getElementById("nav-bar1").style.backgroundColor = "black";
+    document.getElementById("nav-bar3").style.backgroundColor = "black";
+}
+
+function closeNav() {
+    document.getElementById("nav-drawer-container").style.width = "0";
 }
